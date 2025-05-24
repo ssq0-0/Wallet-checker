62a24897cfc13ae377533374e976eb2af1957f44
ssq0-0 <abrehu15k@gmail.com>
2025-05-24 17:08:59 +0300

    Minor change

--- internal/infrastructure/wasmClient/consts.go
+++ internal/infrastructure/wasmClient/consts.go
@@ -3,7 +3,7 @@ package wasmClient
 const (
 	signTypeRegular   = 100120
 	signTypeSecondary = 100121
-	instanceCount     = 1000
-	poolSize          = 1000
+	instanceCount     = 100
+	poolSize          = 100
 	seedOffset        = 4488
 )

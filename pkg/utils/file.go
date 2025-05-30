0943eae7d259d6ea7a4fe964304f510e37f9b815
ssq0-0 <abrehu15k@gmail.com>
2025-05-30 23:29:39 +0300

    update utils

--- pkg/utils/file.go
+++ pkg/utils/file.go
@@ -3,9 +3,22 @@ package utils
 import (
 	"bufio"
 	"os"
+	"path/filepath"
 )
 
+// FileReader reads a file line by line and returns its contents as a slice of strings.
+// It handles different line endings (\n, \r\n) and is cross-platform compatible.
 func FileReader(filename string) ([]string, error) {
+	filename = filepath.Clean(filename)
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return nil, err
+	}
+
+	if _, err := os.OpenFile(filename, os.O_RDONLY, 0); err != nil {
+		return nil, err
+	}
+
 	var lines []string
 	file, err := os.Open(filename)
 	if err != nil {
@@ -15,7 +28,10 @@ func FileReader(filename string) ([]string, error) {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		line := scanner.Text()
+		if line != "" {
+			lines = append(lines, line)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {

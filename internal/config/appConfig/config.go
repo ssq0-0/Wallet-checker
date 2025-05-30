0ff319f947dcdfb783f642a7950e95a6372f3a8a
ssq0-0 <abrehu15k@gmail.com>
2025-05-30 23:19:38 +0300

    Update config structure. Add address file path, replace proxyfile path and add server port

--- internal/config/appConfig/config.go
+++ internal/config/appConfig/config.go
@@ -3,12 +3,15 @@ package appConfig
 type Config struct {
 	Concurrency int      `json:"concurrency" mapstructure:"concurrency"`
 	LoggerLevel string   `json:"logger_level" mapstructure:"logger_level"`
+	ServerPort  string   `json:"server_port" mapstructure:"server_port"`
 	Checkers    Checkers `json:"checkers_params" mapstructure:"checkers_params"`
 }
 
 type Checkers struct {
-	Debank *CheckerSettings `json:"debank" mapstructure:"debank"`
-	Rabby  *CheckerSettings `json:"rabby" mapstructure:"rabby"`
+	AddressFilePath string           `json:"address_file_path" mapstructure:"address_file_path"`
+	ProxyFilePath   string           `json:"proxy_file_path" mapstructure:"proxy_file_path"`
+	Debank          *CheckerSettings `json:"debank" mapstructure:"debank"`
+	Rabby           *CheckerSettings `json:"rabby" mapstructure:"rabby"`
 }
 
 type CheckerSettings struct {
@@ -16,6 +19,5 @@ type CheckerSettings struct {
 	Endpoints       map[string]string `json:"endpoints" mapstructure:"endpoints"`
 	UseProxyPool    bool              `json:"use_proxy_pool" mapstructure:"use_proxy_pool"`
 	RotateProxy     bool              `json:"rotate_proxy" mapstructure:"rotate_proxy"`
-	ProxyFilePath   string            `json:"proxy_file_path" mapstructure:"proxy_file_path"`
 	ContextDeadline int               `json:"deadline_request" mapstructure:"deadline_request"`
 }
